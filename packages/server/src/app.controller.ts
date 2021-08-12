729ab8aac6887c22d1f089fd15597483e416f6ec
RileySeaburg <riley@rileyseaburg.com>
2021-08-11 22:51:44 -0400

    Add server to repo

---
+++ packages/server/src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(@Res() res: Response) {
+    res.status(HttpStatus.OK).json(this.appService.getHello());
+  }
+}
